69a695634c755701f9e564c7c47f8512d63f8160
Jason Grammenos <jgrammenos@gmail.com>
2026-01-14 21:44:22 -0500

    version 0.9.2

--- change_version.sh
+++ change_version.sh
@@ -2,8 +2,8 @@
 #     Rémi Dutil (2026) - updated for ManaDesk creation and Eclipse 2.0 migration
 #!/bin/bash
 
-OLD_VERSION=0.9.0
-NEW_VERSION=0.9.1
+OLD_VERSION=0.9.1
+NEW_VERSION=0.9.2
 find . -name "*.xml" | xargs sed -i -e "s/$OLD_VERSION-SNAPSHOT/$NEW_VERSION-SNAPSHOT/" -e "s/$OLD_VERSION.qualifier/$NEW_VERSION.qualifier/"
 find . -name "*.MF" | xargs sed -i -e "s/$OLD_VERSION.qualifier/$NEW_VERSION.qualifier/"
 find . -name "*.properties" | xargs sed -i -e "s/$OLD_VERSION/$NEW_VERSION/"
