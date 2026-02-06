55bf36099c27c774b6b627f47a908dc23a49452b
BKurler <rdutil@hotmail.com>
2026-02-06 17:23:59 -0500

    potential fix

--- com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/FolderPageGroup.java
+++ com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/FolderPageGroup.java
@@ -59,7 +59,6 @@ public class FolderPageGroup extends ViewPageGroup {
 		final IViewPage page = vc.getViewPage();
 		final CTabItem item = new CTabItem(folder, SWT.CLOSE);
 		item.setText(vc.getName());
-		item.setShowClose(false);
 		page.init(getViewPart());
 		page.createContents(folder);
 		item.setControl(page.getControl());
